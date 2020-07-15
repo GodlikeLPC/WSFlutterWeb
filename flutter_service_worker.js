db14ae869ab754dffba30cd4572bd2dcf2b0514e
lipeichi <lipeichi@wusongtech.com>
2020-07-15 17:55:27 +0800

    first commit

---
+++ flutter_service_worker.js
@@ -0,0 +1,37 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "index.html": "183739345f91ff8ff906838c30227022",
+"/": "183739345f91ff8ff906838c30227022",
+"main.dart.js.deps": "b9aa757d8e2fc593248f6a94d6b9f263",
+"main.dart.js": "07c76e629a686df01f4b3d4c8a867579",
+"assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
