aa944fc6b3155aebe767568787e61fb33d395d31
GodlikeLPC <godlikelpc@126.com>
2020-09-02 18:24:02 +0800

    c

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "aef7d572e0d1ba9047c9180a836bbf4a",
 "/": "aef7d572e0d1ba9047c9180a836bbf4a",
-"main.dart.js": "d4c40b944981759d9410c80a48ba4a1b",
+"main.dart.js": "be826d8f261666328a16311fe351f6cd",
 "assets/LICENSE": "0000d255865246c9b55862d96ffd3089",
 "assets/images/close_nav.png": "cc0b7ea75df3a83d452ebb94dc9293a2",
 "assets/AssetManifest.json": "c14c67f0a16046fc693f803b070ff9f0",
